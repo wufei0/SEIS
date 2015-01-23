a0a6cf39cd96fe10cab6e6534ba6cc843d9519cd
Norman Apilado Cabilatazan <norman.cabilatazan@yahoo.com>
2015-01-23 10:02:52 +0800

    updated-paginationGroup

--- maintenance/security/group.php
+++ maintenance/security/group.php
@@ -126,27 +126,6 @@
                                                           
                                                     </div>
                                                 </div>
-                                                <div class="col-md-8">
-                                            <nav>
-
-<!---------------pagination--------------->
-                                              <ul class="rev-pagination pagination">
-                                                <li><a href="#"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>
-                                                <li><a href="#">1</a></li>
-                                                <li><a href="#">2</a></li>
-                                                <li><a href="#">3</a></li>
-                                                <li><a href="#">4</a></li>
-                                                <li><a href="#">5</a></li>
-                                                <li><a href="#">5</a></li>
-                                                <li><a href="#">5</a></li>
-                                                <li><a href="#">5</a></li>
-                                   
-                                                <li><a href="#"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>
-                                              </ul>
-<!---------------end pagination--------------->
-
-                                            </nav>
-                                                    </div>
                                             </div>
                                         </div>
                                         </div>
