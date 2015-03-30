f0ff236c5a777ccb94ba3b4e14444e3643e3d924
Norman Apilado Cabilatazan <norman.cabilatazan@yahoo.com>
2015-03-30 09:10:50 +0800

    Equipment Module & Search Fixed (Maintenance)

---
+++ equipment/modal.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="myModalovermodal"  aria-labelledby="myModalLabel" >
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" ><div id="modalTitleovermodal"></div></h4>
+      </div>
+
+      <div id="modalContentovermodal" class="modal-body">
+
+      </div>
+
+      <div class="modal-footer">
+        <div id="footerNoteovermodal">
+        </div>
+          <div id="modalButtonovermodal">
+              <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+          </div>
+
+
+      </div>
+
+    </div>
+  </div>
+</div>
